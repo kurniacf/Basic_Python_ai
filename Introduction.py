c3353a9abb1a24afcd4e84f58124181e2cf8c20d
kurniacf <kcf170202@gmail.com>
2021-01-09 14:34:40 +0700

    Meeting Day-1

---
+++ Introduction.py
@@ -0,0 +1,38 @@
+print("--- WELCOME ---")
+print()
+
+# variable
+print("Variable")
+x = 5
+y = "Hello World!"
+print(x)
+print(y, "\n")
+
+# Data Types
+print("Data Types")
+x = 5
+y = 4.5
+z = "hello world"
+o = True
+
+print(type(x), x)
+print(type(y), y)
+print(type(z), z)
+print(type(o), o, "\n")
+
+# Math Operations
+print("Math Operations")
+x = 5
+y = 2
+print(x, "+", y, "=", x+y)
+print(x, "-", y, "=", x-y)
+print(x, "*", y, "=", x*y)
+print(x, "/", y, "=", x/y)
+print(x, "%", y, "=", x % y)
+print(x, "**", y, "=", x**y, "\n")
+
+# Casting
+print("Casting")
+print(int(2.8))
+print(float("4"))
+print(str(4.0))
