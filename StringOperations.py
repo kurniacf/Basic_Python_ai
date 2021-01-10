ff4e3550a7330c4b6f19a440c5c4dc527df6a25b
kurniacf <kcf170202@gmail.com>
2021-01-10 13:23:57 +0700

    commit

---
+++ StringOperations.py
@@ -0,0 +1,9 @@
+# 0 1 2 3 4 5 etc.
+a = "hello world!"
+print(a[1])  # ambil 1 char
+
+b = "hello world!"
+print(a[2:5])  # ambil char dari x hingga y (x:y)
+
+c = "hello world!"
+print(len(c))  # menghitung panjang string
---
+++ UserInput.py
@@ -0,0 +1,7 @@
+name = input("Enter Name: ")
+print("Your name is: " + name)
+print(type(name))
+
+number = int(input("Enter Number: "))
+print("Your number is: ", number)
+print(type(number))
